06e4aa93419538022d789246c134b8acae8d9f74
Pranav Arora <arora.pranav.2000@gmail.com>
2021-07-04 18:04:36 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider, createMuiTheme } from "@material-ui/core"
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import Home from './pages/Home';
+
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#85a719'
+    },
+    secondary: {
+      main: '#e6ea44'
+    },
+    info: {
+      main: '#625638'
+    }
+  }
+})
 
 function App() {
   return (
     <>
-    
+      <ThemeProvider theme={theme}>
+        <Router>
+          <Switch>
+            <Route path='/'>
+              <Home /> 
+            </Route>  
+          </Switch>  
+        </Router>
+      </ThemeProvider>  
     </>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Vendors from '../Components/Vendors';
+
+
+
+function Home() {
+    return (
+        <div>
+            <Vendors />
+        </div>
+    );
+}
+
+export default Home;
